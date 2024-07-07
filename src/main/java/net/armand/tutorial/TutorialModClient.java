0de3915f821ec8910a8062121de3e6afe4e52a60
Admany <vapsvaarmandas@gmail.com>
2024-07-07 17:24:50 +0300

    Update

---
+++ src/main/java/net/armand/tutorial/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.armand.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
